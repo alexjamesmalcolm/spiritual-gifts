d9e381cc65b7ee6636d8aaf3e6a33b517a72b7fa
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-10-08 01:04:36 -0400

    Removed useVerse from useCategories

--- src/hooks/useCategories/index.tsx
+++ src/hooks/useCategories/index.tsx
@@ -28,83 +28,15 @@ export interface Category {
   shortName: string;
 }
 
-interface Common {
-  version: string;
-  type: string;
-  direction: string;
-}
-
-interface ChapterContent {
-  [key: number]: {
-    verse: string;
-    verse_nr: number;
-  };
-}
-
-interface Chapter extends Common {
-  type: "chapter";
-  book_name: string;
-  book_nr: number;
-  chapter_nr: number;
-  chapter: ChapterContent;
-}
-
-interface Book {
-  book_name: string;
-  book_nr: string;
-  book_ref: string;
-  chapter_nr: string;
-  chapter: ChapterContent;
-}
-
-interface Verse extends Common {
-  type: "verse";
-  book: Book[];
-}
-
-const Verse = ({ verse }) => {
-  const getResource = useCallback(
-    () =>
-      fetch(`/verse/json?p=${verse.replaceAll(" ", "")}&v=weymouth`)
-        .then((response) => response.text())
-        .then((text) => JSON.parse(text.slice(1, -2))),
-    [verse]
-  );
-  const {
-    data,
-    isLoading,
-    error,
-  }: {
-    data: Chapter | Verse | undefined | null;
-    isLoading: boolean;
-    error: Error | false;
-  } = useResource(verse, { getResource });
-  const verseContent = useMemo<string>(() => {
-    if (isLoading || !data || error) {
-      return "";
-    }
-    if (data.type === "chapter") {
-      return (Object.entries(data.chapter).map(
-        ([, { verse }]) => verse
-      ) as string[]).join("");
-    } else if (data.type === "verse") {
-      return (data.book
-        .flatMap((book) => Object.entries(book.chapter))
-        .map(([, { verse }]) => verse) as string[]).join("");
-    }
-    return "";
-  }, [data, error, isLoading]);
-  return (
-    <a
-      href={`https://www.biblegateway.com/passage/?search=${verse}&version=NASB`}
-      rel="noreferrer noopener"
-      target="_blank"
-      title={verseContent}
-    >
-      {verse}
-    </a>
-  );
-};
+const Verse = ({ verse }) => (
+  <a
+    href={`https://www.biblegateway.com/passage/?search=${verse}&version=NASB`}
+    rel="noreferrer noopener"
+    target="_blank"
+  >
+    {verse}
+  </a>
+);
 
 const useCategories = (): Category[] =>
   useMemo<Category[]>(
