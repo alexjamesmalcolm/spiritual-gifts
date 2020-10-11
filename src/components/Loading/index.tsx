b6db6b1c0d5511ffec99cf9bc79e022538dbd7d7
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-10-11 02:03:32 -0400

    Swapped out browser defined progress bar for css spinners

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./Progress.module.css";
+import { Refreshing } from "css-spinners-react";
 
 const Loading = () => (
   <div className={styles.container}>
-    <progress className={styles.progress} />
+    <Refreshing />
   </div>
 );
 
