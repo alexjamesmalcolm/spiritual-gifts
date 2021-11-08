5695836e129e09b2e4b6d751780fc5edab1303f5
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2021-11-08 08:57:01 -0500

    Created a test for ValidResults

---
+++ src/pages/Results/components/ValidResults/index.test.tsx
@@ -0,0 +1,24 @@
+import { render } from "@testing-library/react";
+import { Result } from "hooks/useResults";
+import Provider from "Provider";
+import ValidResults from ".";
+
+describe("ValidResults", () => {
+  it("render a single result", () => {
+    const results: Result[] = [
+      {
+        description: <p>Description</p>,
+        name: "Administration",
+        rank: 1,
+        score: 1,
+        shortName: "Admin",
+      },
+    ];
+    const { container } = render(
+      <Provider>
+        <ValidResults results={results} />
+      </Provider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
