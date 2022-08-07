d9f164d7ca3f9af09e3127dc8008c82ec02b3e6b
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2022-08-07 14:56:22 -0400

    Added persistent state of form using localStorage https://stackoverflow.com/questions/68375621/saving-a-redux-state-to-localstorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,22 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import answersReducer from "hooks/useAnswers/reducer";
 import serviceWorkerReducer from "hooks/useServiceWorker/reducer";
 
+const persistentStateKey = "state";
+const localStorageState = localStorage.getItem(persistentStateKey);
+const persistentState = localStorageState ? JSON.parse(localStorageState) : {};
+
 export const store = configureStore({
   reducer: {
     answers: answersReducer,
     serviceWorker: serviceWorkerReducer,
   },
+  preloadedState: persistentState,
+});
+
+store.subscribe(() => {
+  const state = store.getState();
+  const serializedState = JSON.stringify(state);
+  localStorage.setItem("state", serializedState);
 });
 
 export type RootState = ReturnType<typeof store.getState>;
